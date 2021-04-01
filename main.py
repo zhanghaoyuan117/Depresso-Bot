ae0b361e3dd4f87862d072fd345529a94834f6aa
Marc <zhanghaoyuan117@hotmail.com>
2021-04-01 15:16:57 -0400

    Added The Stock python file Created environment variable for discord bot token

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import os
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='+');
-
+BOT_KEY = os.environ.get('BOT_KEY')
 
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send('Invalid command used.')
 
-#hello
+
 
 @client.command()
 async def load(ctx, extension):
@@ -32,4 +32,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('ODEzMTM1NzE0NTMwODg1NjMy.YDK5pQ.O9J80Nvo5kzg38LrJ5JTylrGHnE')
+client.run(BOT_KEY)
